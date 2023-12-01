55c8e412a8f011058e3939d53afe390506f34c9b
Luca Addario <iamlucaaddario@gmail.com>
2023-12-01 13:19:25 +0100

    Header Dinamico

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,40 @@
 
 //SWIPER CAROUSEL WELCOME
 var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: false,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 0,
-      modifier: 0,
-      slideShadows: false,
-    },
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: false,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 0,
+    modifier: 0,
+    slideShadows: false,
+  },
+});
+
+/*------------------
+Header Dinamico
+-------------------*/
+//Cattura dei Link con data-title
+const titleLinkElements = document.querySelectorAll('#swiperData');
+//Cattura del titolo, sottotitolo e contenuto dell'header
+let titleHeader = document.querySelector('#titleHeader');
+
+//ForEach di tutti i links
+titleLinkElements.forEach((link)=> {
+  //Al click di ciascuno dei link
+  link.addEventListener('click', function () {
+
+    // Recupera il titolo e contenuto dal data attributo
+    const title = link.dataset.title;
+    const content = link.dataset.desc;
+    const image = link.dataset.image;
+
+    // Aggiorna dinamicamente l'header con i dati del titolo
+    titleHeader.innerText = title;
+    contentHeader.innerText = content;
+    document.querySelector('#header').style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.432), rgb(0, 0, 0)), url('${image}')`;
   });
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     @endif
     {{-- INIZIO HEADER --}}
     <header>
-        <div class="p-5 header-bg">
+        <header class="p-5" style="background: linear-gradient(rgba(0, 0, 0, 0.432), rgb(0, 0, 0)), url('media/header_default.jpg'); min-height: 85vh; max-height: 85vh; background-repeat: no-repeat; background-position: center center; background-size: contain;"  id="header">
 
             <div class="row text-white text-start text-lg-end ms-2 ms-lg-0 me-lg-3 fs-3">
                 <div class="col-12">
@@ -26,11 +26,10 @@
             </div>
 
             <div class="d-flex justify-content-start align-items-center h-100">
-                <div class="text-white">
-                    <h1 class="fw-bold ms-2 display-1 text-uppercase">One Piece</h1>
-                    <h4 class="ms-3 text-uppercase header-subtitle col-lg-12 fw-bold ">Saga di headeggs</h4>
+                <div class="text-white contentheader">
+                    <h1 class="fw-bold ms-2 display-6 text-uppercase" id="titleHeader">One Piece</h1>
                     <div class="col-10 col-md-8 col-lg-6 col-xl-4 col-xl-4">
-                        <p class="ms-3 mt-3 header-content">
+                        <p class="ms-3 mt-3 header-content" id="contentHeader">
                             {{Str::limit("One Piece è un avvincente manga e anime creato da Eiichiro Oda. La storia segue Monkey D. Luffy, un giovane con il desiderio di diventare il Re dei Pirati, mentre si imbarca in un'epica avventura attraverso mari pericolosi. Luffy ottiene poteri da un frutto...", 250)}}
                         </div>
                         <a class="btn btn-header btn-light ms-3 mt-2 px-3 py-2" href="#!" role="button">
@@ -49,7 +48,7 @@
                     <div class="swiper-wrapper">
                         @foreach ($products as $product)
                         <div class="swiper-slide me-5">
-                            <a href="">
+                            <a href="#" data-title="{{$product->title}}" data-desc="{{$product->description}}" data-image="{{Storage::url($product->image)}}" id="swiperData">
                                 <img src="{{Storage::url($product->image)}}"/>
                             </a>
                         </div>
@@ -69,7 +68,7 @@
                         <div class="swiper-wrapper">
                             @foreach ($productsByFilms as $product)
                             <div class="swiper-slide me-5">
-                                <a href="">
+                                <a href="#" data-title="{{$product->title}}" data-desc="{{$product->description}}" data-image="{{Storage::url($product->image)}}" id="swiperData">
                                     <img src="{{Storage::url($product->image)}}"/>
                                 </a>
                             </div>
@@ -85,7 +84,7 @@
                         <div class="swiper-wrapper">
                             @foreach ($productsBySeries as $product)
                             <div class="swiper-slide me-5">
-                                <a href="">
+                                <a href="#" data-title="{{$product->title}}" data-desc="{{$product->description}}" data-image="{{Storage::url($product->image)}}" id="swiperData">
                                     <img src="{{Storage::url($product->image)}}"/>
                                 </a>
                             </div>
@@ -102,7 +101,7 @@
                         <div class="swiper-wrapper">
                             @foreach ($productsByAnime as $product)
                             <div class="swiper-slide me-5">
-                                <a href="">
+                                <a href="#" data-title="{{$product->title}}" data-desc="{{$product->description}}" data-image="{{Storage::url($product->image)}}" id="swiperData">
                                     <img src="{{Storage::url($product->image)}}"/>
                                 </a>
                             </div>
